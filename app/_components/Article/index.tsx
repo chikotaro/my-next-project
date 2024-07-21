f2713d6e4d1b08f80a4d1c62f59fbef48ed6defb
chikotaro <140727421+chikotaro@users.noreply.github.com>
2024-07-21 22:34:59 +0900

    ♻️ ?? ""でエラーをなくす

--- app/_components/Article/index.tsx
+++ app/_components/Article/index.tsx
@@ -1,61 +1,63 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import type { News } from '@/app/_libs/microcms';
-import Date from '../Date';
-import Category from '../Category';
-import styles from './index.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import type { News } from "@/app/_libs/microcms";
+import Date from "../Date";
+import Category from "../Category";
+import styles from "./index.module.css";
 
 type Props = {
-  data: News;
+	data: News;
 };
 
-
 interface ArticleProps {
-  data: {
-    category: {
-      id: string;
-      // 他のカテゴリーのフィールド
-    } | null;
-    // 他のデータのフィールド
-  };
+	data: {
+		category: {
+			id: string;
+			// 他のカテゴリーのフィールド
+		} | null;
+		// 他のデータのフィールド
+	};
 }
 
 export default function Article({ data }: Props) {
-  return (
-    <main>
-      <h1 className={styles.title}>{data.title}</h1>
-      <p className={styles.description}>{data.description}</p>
-      <div className={styles.meta}>
-      {data.category ? (
-        <Link href={`/news/category/${data.category.id}`} className={styles.categoryLink}>
-          <Category category={data.category} />
-        </Link>
-      ) : (
-        <div className={styles.categoryLink}>カテゴリーが利用できません</div>
-      )}
-        {/* <Link
+	return (
+		<main>
+			<h1 className={styles.title}>{data.title}</h1>
+			<p className={styles.description}>{data.description}</p>
+			<div className={styles.meta}>
+				{data.category ? (
+					<Link
+						href={`/news/category/${data.category.id}`}
+						className={styles.categoryLink}
+					>
+						<Category category={data.category} />
+					</Link>
+				) : (
+					<div className={styles.categoryLink}>カテゴリーが利用できません</div>
+				)}
+				{/* <Link
           href={`/news/category/${data.category.id}`}
           className={styles.categoryLink}
         >
           <Category category={data.category} />
         </Link> */}
-        <Date date={data.publishedAt ?? data.createdAt} />
-      </div>
-      {data.thumbnail && (
-        <Image
-          src={data.thumbnail.url}
-          alt=""
-          className={styles.thumbnail}
-          width={data.thumbnail.width}
-          height={data.thumbnail.height}
-        />
-      )}
-      <div
-        className={styles.content}
-        dangerouslySetInnerHTML={{
-          __html: data.content,
-        }}
-      />
-    </main>
-  );
+				<Date date={data.publishedAt ?? data.createdAt} />
+			</div>
+			{data.thumbnail && (
+				<Image
+					src={data.thumbnail.url}
+					alt=""
+					className={styles.thumbnail}
+					width={data.thumbnail.width}
+					height={data.thumbnail.height}
+				/>
+			)}
+			<div
+				className={styles.content}
+				dangerouslySetInnerHTML={{
+					__html: data.content ?? "",
+				}}
+			/>
+		</main>
+	);
 }
