78238d6e938127880b8b1b87113555d3498895d8
chikotaro <140727421+chikotaro@users.noreply.github.com>
2024-07-24 12:45:43 +0900

    mojiの変更

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,15 @@
 import ContactForm from "../_components/contactForm";
 import styles from "./page.module.css";
 
-export default function Contact(){
-return (
-  <div className={styles.container}> 
-    <p className={styles.text}>
-      ご質問、ご相談は下記フォームよりお問い合わせください。
-      <br />
-      内容確認後、担当者より通常3日以内にご連絡いたします。
-    </p>
-    <ContactForm />
-  </div>
-)
-} 
+export default function Contact() {
+	return (
+		<div className={styles.container}>
+			<p className={styles.text}>
+				ご質問、ご相談は下記フォームよりお問い合わせください。
+				<br />
+				内容確認後、担当者より通常3日以内にご連絡いたします。
+			</p>
+			<ContactForm />
+		</div>
+	);
+}
